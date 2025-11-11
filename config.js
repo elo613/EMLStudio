ebaf66f5c3ebb29caf1380c67208ef585e3a3e6d
elo613 <75640827+elo613@users.noreply.github.com>
2025-11-11 17:37:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,21 @@
-// Initialize Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "[YOUR_API_KEY_HERE]",
-    authDomain: "[YOUR_PROJECT_ID].firebaseapp.com",
-    projectId: "[YOUR_PROJECT_ID]",
-    storageBucket: "[YOUR_PROJECT_ID].appspot.com",
-    messagingSenderId: "[YOUR_SENDER_ID]",
-    appId: "[YOUR_APP_ID]"
+  apiKey: "AIzaSyCDXSMBQcy5x0SHnUtxbsiKhP5jAoMxu3Q",
+  authDomain: "eml-studios.firebaseapp.com",
+  projectId: "eml-studios",
+  storageBucket: "eml-studios.firebasestorage.app",
+  messagingSenderId: "557429328885",
+  appId: "1:557429328885:web:8fdb2da1ba7947f52aeebc",
+  measurementId: "G-HRXCP4XQWP"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-// The firebase variables (auth and db) are now available globally 
-// and used in the other scripts (auth.js and home.js)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
